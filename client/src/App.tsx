305cc95077080889d492328130be28e5091df37e
sgdg <seungdeng17@gmail.com>
2021-09-22 18:08:33 +0900

    init

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { Route, Switch } from 'react-router-dom';
 import GlobalStyles from '@style/GlobalStyles';
-import Sample from '@/Sample';
+
+import Header from '@components/Header/Header';
+import Main from '@page/Main';
 
 export default function App() {
   return (
     <>
       <GlobalStyles />
+      <Header />
       <Switch>
-        <Route exact path="/" component={Sample} />
+        <Route exact path="/" component={Main} />
       </Switch>
     </>
   );
--- server/app.js
+++ server/app.js
@@ -1,12 +1,10 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const { db } = require('./db');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.db = db;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(
