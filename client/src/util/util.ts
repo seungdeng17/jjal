7dcffaa88a4f5fb788c55ad9bda621b7c7fa27ab
이승택 <seungdeng17@gmail.com>
2021-09-26 18:42:29 +0900

    feat: 로그아웃

--- client/src/util/util.ts
+++ client/src/util/util.ts
@@ -1,1 +1,8 @@
-export const defer = (ms: number) => new Promise((res) => setTimeout(res, ms));
+import Progress from 'rsup-progress';
+
+const progress = new Progress({
+  color: '#54a0ff',
+  zIndex: 100000,
+});
+
+export const defer = (ms: number) => progress.promise(new Promise((res) => setTimeout(res, ms)));
