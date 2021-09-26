838b7605842ad7117351d08bbf7a636f7288f902
이승택 <seungdeng17@gmail.com>
2021-09-26 17:21:36 +0900

    feat: 로그인 정보 상태

--- client/src/store/login.ts
+++ client/src/store/login.ts
@@ -1,8 +1,19 @@
 import { atom } from 'recoil';
 
+type LoginInfo = {
+  name: string;
+};
+
 export const loginState = atom<boolean>({
   key: 'isLogin',
   default: false,
+});
+
+export const loginInfoState = atom<LoginInfo>({
+  key: 'loginInfo',
+  default: {
+    name: '',
+  },
 });
 
 export const adminState = atom<boolean>({
